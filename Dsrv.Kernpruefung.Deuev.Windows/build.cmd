d801befe702980b9a6dd466a5e24595d3891098e
Mark Junker <m.junker@fubar-dev.de>
2015-05-05 12:00:54 +0200

    Build-Script wurde nicht angepasst

--- Dsrv.Kernpruefung.Deuev.Windows/build.cmd
+++ Dsrv.Kernpruefung.Deuev.Windows/build.cmd
@@ -1,3 +1,3 @@
 @echo off
-SET IKVM_PATH=%~dp0\..\ikvm-8.1.5561
+SET IKVM_PATH=%~dp0\..\ikvm-8.0.5449.1
 "%IKVM_PATH%\bin\ikvmc.exe" -target:library "-out:%~dp0\Dsrv.Kernpruefung.Deuev.dll" "%~dp0\..\Kernpruefung\kernpruefung_deuev_jar.jar" -version:%1 -fileversion:%2
