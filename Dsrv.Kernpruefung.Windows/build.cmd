f5986b32c527a0fb47a5570f567ec70a3ae6b0d2
Mark Junker <m.junker@fubar-dev.de>
2015-05-05 11:22:25 +0200

    Müll löschen

--- Dsrv.Kernpruefung.Windows/build.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-SET IKVM_PATH=%~dp0\..\ikvm-8.1.5561
-"%IKVM_PATH%\bin\ikvmc.exe" -target:library "-out:%~dp0\Deuev.Kernpruefung.dll" "%~dp0\..\Kernpruefung\kernpruefung_deuev_jar.jar" -version:%1 -fileversion:%2
