901845e1471695ae74f358295730a6a9aba7d412
Mark Junker <m.junker@fubar-dev.de>
2015-05-05 15:31:33 +0200

    Der Adapter wird Teil der DLL

--- Dsrv.Kernpruefung.Deuev.Windows/build.cmd
+++ Dsrv.Kernpruefung.Deuev.Windows/build.cmd
@@ -1,3 +1,4 @@
 @echo off
 SET IKVM_PATH=%~dp0\..\ikvm-8.0.5449.1
-"%IKVM_PATH%\bin\ikvmc.exe" -target:library "-out:%~dp0\Dsrv.Kernpruefung.Deuev.dll" "%~dp0\..\Kernpruefung\kernpruefung_deuev_jar.jar" -version:%1 -fileversion:%2
+"%IKVM_PATH%\bin\ikvmc.exe" -version:%1 -fileversion:%2 -target:library "-out:%~dp0\Dsrv.Kernpruefung.Deuev.dll" "%~dp0\..\Kernpruefung\kernpruefung_deuev_jar.jar" "%~dp0\..\Kernpruefung\kernpruefung_deuev_adapter.jar"
+REM -debug "-srcpath:%~dp0\..\Kernpruefung\kernpruefung_deuev_jar"
