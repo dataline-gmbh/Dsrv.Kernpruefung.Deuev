1de63b67c3da01ba773bc0bb970e02e3c320e52c
DBarthel <d.barthel@dataline.de>
2016-07-01 12:07:00 +0200

    Name geändert

--- build-1.8.cmd
+++ build-1.8.cmd
@@ -5,4 +5,4 @@ SET JAVA_PACKAGE_VERSION=1_8
 SET BUILD=%1
 SET BASE_PATH=%~dp0
 SET IKVM_PATH=%BASE_PATH%\ikvm-8.0.5449.1
-"%IKVM_PATH%\bin\ikvmc.exe" -version:%FULL_PACKAGE_VERSION% -fileversion:%FULL_PACKAGE_VERSION%.%BUILD% -target:library "-out:%BASE_PATH%\NuGet\%PACKAGE_VERSION%\Dsrv.Kernpruefung.Deuev-%PACKAGE_VERSION%.dll" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_%JAVA_PACKAGE_VERSION%_jar.jar" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_adapter_1_1_2.jar"
+"%IKVM_PATH%\bin\ikvmc.exe" -version:%FULL_PACKAGE_VERSION% -fileversion:%FULL_PACKAGE_VERSION%.%BUILD% -target:library "-out:%BASE_PATH%\NuGet\%PACKAGE_VERSION%\Dsrv.Kernpruefung.Deuev-%PACKAGE_VERSION%.dll" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_%JAVA_PACKAGE_VERSION%_jar.jar" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_adapter_1_1_2_jar.jar"
