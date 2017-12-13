b8b339b853254b52e354eb74b65233a25fa9b790
Jan Hindermann <j.hindermann@dataline.de>
2017-12-13 14:46:32 +0100

    Neue DEÜV Kernprüfung Version 1.11.1

---
+++ build-1.11.cmd
@@ -0,0 +1,8 @@
+@echo off
+SET PACKAGE_VERSION=1.11
+SET FULL_PACKAGE_VERSION=1.11.1
+SET JAVA_PACKAGE_VERSION=1_11_1
+SET BUILD=%1
+SET BASE_PATH=%~dp0
+SET IKVM_PATH=%BASE_PATH%\ikvm-8.0.5449.1
+"%IKVM_PATH%\bin\ikvmc.exe" -version:%FULL_PACKAGE_VERSION% -fileversion:%FULL_PACKAGE_VERSION%.%BUILD% -target:library "-out:%BASE_PATH%\NuGet\%PACKAGE_VERSION%\Dsrv.Kernpruefung.Deuev-%PACKAGE_VERSION%.dll" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_%JAVA_PACKAGE_VERSION%_jar.jar" "%BASE_PATH%\Kernpruefung\kernpruefung_deuev_adapter_1_1_2_jar.jar"
